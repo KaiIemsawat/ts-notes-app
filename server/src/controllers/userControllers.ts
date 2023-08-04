ed735a6e32bf37c864f73299dec8cd7ebaf7f35f
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-08-03 21:08:04 -0400

    logout completed

--- server/src/controllers/userControllers.ts
+++ server/src/controllers/userControllers.ts
@@ -3,6 +3,22 @@ import createHttpError from "http-errors";
 import UserModel from "../models/userModel";
 import bcrypt from "bcrypt";
 
+export const getAuthenticatedUser: RequestHandler = async (req, res, next) => {
+    const authenticatedUserId = req.session.userId;
+
+    try {
+        if (!authenticatedUserId) {
+            throw createHttpError(401, "User ont authenticated");
+        }
+        const user = await UserModel.findById(authenticatedUserId)
+            .select("+email")
+            .exec();
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
+};
+
 interface SignUpBody {
     username?: string;
     email?: string;
@@ -65,4 +81,55 @@ export const signUp: RequestHandler<
     } catch (error) {
         next(error);
     }
+};
+
+interface LoginBody {
+    username?: string;
+    password?: string;
+}
+
+export const login: RequestHandler<
+    unknown,
+    unknown,
+    LoginBody,
+    unknown
+> = async (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        if (!username || !password) {
+            throw createHttpError(400, "Missing parameter(s)");
+        }
+        const user = await UserModel.findOne({ username: username })
+            .select("+password +email")
+            .exec();
+
+        if (!user) {
+            throw createHttpError(401, "Invalid credentials");
+        }
+
+        // compare input password and password in database
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordMatch) {
+            throw createHttpError(401, "Invalid credentials");
+        }
+
+        req.session.userId = user._id;
+        res.status(201).json(user);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const logout: RequestHandler = (req, res, next) => {
+    req.session.destroy((error) => {
+        // .destroy() < -- to detroy the session
+        if (error) {
+            next(error);
+        } else {
+            res.sendStatus(200); // <-- use sendStatus instead of .json() since we don't have json body
+        }
+    });
 };
